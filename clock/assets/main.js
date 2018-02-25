d2fd02b8ddb480d78c6d08084958e334bb66775e
Arfaqur-Rahman <arfaqur@gmail.com>
2018-02-25 10:59:47 +0600

    clock

---
+++ clock/assets/main.js
@@ -0,0 +1,35 @@
+﻿const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate(){
+    
+    const now= new Date();
+    
+    const seconds = now.getSeconds();
+    
+    const secondsDegrees= ((seconds/60)*360+90);
+    
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    
+    //minute's hand
+    
+    const mins = now.getMinutes();
+    
+    const minsDegrees = ((mins/60)*360+90);
+    
+     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    
+    //hour's hand
+    
+    const hour = now.getHours();
+    
+    const hourDegrees = ((hour/12)*360+90);
+    
+     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    
+    
+}
+
+setInterval(setDate, 1000);
